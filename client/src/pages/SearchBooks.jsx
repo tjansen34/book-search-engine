60cf0491e37e15a19888c1f17d0fb6fe2e732da9
tess jansen <tjansen34@mckaylas-MBP.attlocal.net>
2024-01-30 16:39:55 -0600

    update routes

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -4,7 +4,7 @@ import { Container, Col, Form, Button, Card, Row } from "react-bootstrap";
 import Auth from "../utils/auth";
 import { searchGoogleBooks } from "../utils/API";
 import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
-import { SAVE_BOOK } from "../mutations";
+import { SAVE_BOOK } from "../utils/mutations";
 
 const SearchBooks = () => {
   const [searchedBooks, setSearchedBooks] = useState([]);
