fd856002dec002290682680d8cff1be076e29b56
tess jansen <tjansen34@mckaylas-MBP.attlocal.net>
2024-01-30 16:04:44 -0600

    update frontend

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -19,17 +19,14 @@ export const ADD_USER = gql`
 export const SAVE_BOOK = gql`
   mutation SaveBook($bookInput: BookInput!) {
     saveBook(bookInput: $bookInput) {
-      // Define the fields you want to retrieve after saving a book
-      // For example: id, title, author, etc.
-    }
+      _id
   }
 `;
 
 export const REMOVE_BOOK = gql`
   mutation RemoveBook($bookId: ID!) {
     removeBook(bookId: $bookId) {
-      // Define the fields you want to retrieve after removing a book
-      // For example: id, title, author, etc.
+      _id
     }
   }
 `;
