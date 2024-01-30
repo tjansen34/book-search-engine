596e821ce0171a955a7ca78367f8c5b5ef71f079
tess jansen <tjansen34@mckaylas-MBP.attlocal.net>
2024-01-30 15:51:26 -0600

    make typedef corrections

--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -8,16 +8,13 @@ const typeDefs = gql`
     userId: ID!
     username: String!
     email: String!
-    # Add other User fields as needed
   }
 
-  # Define the Auth type
   type Auth {
     token: String!
     user: User!
   }
 
-  # Define the input type for saveBook mutation
   input BookInput {
     authors: [String!]!
     description: String!
@@ -27,13 +24,11 @@ const typeDefs = gql`
     link: String!
   }
 
-  # Define the Query type
   type Query {
     me: User!
     # Add other queries as needed
   }
 
-  # Define the Mutation type
   type Mutation {
     login(email: String!, password: String!): Auth!
     addUser(username: String!, email: String!, password: String!): Auth!
