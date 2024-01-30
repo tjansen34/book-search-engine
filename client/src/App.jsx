65ee28b05831f11e540ea1ebb84ffa1191bbba29
tess jansen <tjansen34@mckaylas-MBP.attlocal.net>
2024-01-30 16:18:27 -0600

    update app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    </ApolloProvider>
   );
 }
 
