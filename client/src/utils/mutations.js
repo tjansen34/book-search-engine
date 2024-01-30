d5ff8d6ed5fac48d652a0eec2d8b4879c0e3f2ac
tess jansen <tjansen34@mckaylas-MBP.attlocal.net>
2024-01-30 16:11:45 -0600

    update front end

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,7 +3,10 @@ import { gql } from "@apollo/client";
 export const LOGIN_USER = gql`
   mutation LoginUser($email: String!, $password: String!) {
     loginUser(email: $email, password: $password) {
-        _id
+      _id
+      username
+      email
+      token
     }
   }
 `;
@@ -11,7 +14,10 @@ export const LOGIN_USER = gql`
 export const ADD_USER = gql`
   mutation AddUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
-        _id
+      _id
+      username
+      email
+      token
     }
   }
 `;
@@ -20,6 +26,7 @@ export const SAVE_BOOK = gql`
   mutation SaveBook($bookInput: BookInput!) {
     saveBook(bookInput: $bookInput) {
       _id
+      bookInput
   }
 `;
 
@@ -27,6 +34,7 @@ export const REMOVE_BOOK = gql`
   mutation RemoveBook($bookId: ID!) {
     removeBook(bookId: $bookId) {
       _id
+      bookId
     }
   }
 `;
